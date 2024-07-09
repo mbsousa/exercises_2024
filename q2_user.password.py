56d973b47d0334e1b1cbda19bd1023933ccc3810
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-07-08 21:38:05 -0300

    Update q2_user.password.py

--- q2_user.password.py
+++ q2_user.password.py
@@ -9,14 +9,10 @@ import getpass
 while True:
     user = input("Your username: ")
     password = getpass.getpass("Your password: ")
-    try:
-        user = str(user)
-        if password != user:
-            print("Successfully register.")
-            break
-        else:
-            print("Username and password should not the same. Please, try again.")
-    except:
-        print("Invalid values. Please, try again.")
-
+    if password != user:
+        print("Successfully register.")
+        break
+    else:
+        print("Username and password should not the same. Please, try again.")
+        continue
 
