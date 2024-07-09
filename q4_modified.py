a172db24109c127a42baae1fe9de90673605f036
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-07-08 23:00:47 -0300

    Add files via upload

---
+++ q4_modified.py
@@ -0,0 +1,17 @@
+# Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro.
+# Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+# Make a program that prints the numbers from 1 to 20 on the screen, one below the other.
+# Then modify the program so that it shows the numbers next to each other.
+
+def Append(i):
+    number.append(i)
+
+number = []
+
+for i in range(1,21):
+    Append(i)
+for each in number:
+    print(each)
+for each in number:
+    print(each, end=' ')
