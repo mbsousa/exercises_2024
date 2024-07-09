9238271bdc3db0566527720c29a0e7df190a81b4
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-07-08 21:02:15 -0300

    Update and rename q1_nota.py to q1_note.py

--- q1_note.py
+++ q1_note.py
@@ -1,6 +1,9 @@
 # Faça um programa que peça uma nota, entre zero e dez. 
 # Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. 
 
+# Make a program that asks for a note, between zero and ten. 
+# Show a message if the value is invalid and continue asking until the user enters a valid value.
+
 while True:
     nota = input("Digite sua nota: ")
     try:
