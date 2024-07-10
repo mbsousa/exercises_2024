5abd8ae26dd8dbd241a1b7fa90522f89ae353992
Beatriz <125318073+mbsousa@users.noreply.github.com>
2024-07-10 09:54:33 -0300

    Add files via upload

---
+++ q7_odd.py
@@ -0,0 +1,9 @@
+# Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+# Write a program that prints only odd numbers between 1 and 50 on the screen.
+
+for num in range(1,51):
+    if num % 2 == 0:
+        pass
+    else:
+        print(num)
